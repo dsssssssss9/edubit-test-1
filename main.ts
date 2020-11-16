b1505e9ba7b9cd59764055a37b12cde45a167491
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-11-16 21:07:10 +0000

    add servo 1 control test

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
 input.onButtonPressed(Button.A, function () {
     edubitMotors.runMotor(MotorChannel.M1, MotorDirection.Forward, edubitPotentioBit.readPotValue())
+    edubitMotors.setServoPosition(ServoChannel.S1, 0)
+    basic.pause(200)
+    edubitMotors.disableServo(ServoChannel.All)
 })
 input.onButtonPressed(Button.AB, function () {
     edubitMotors.brakeMotor(MotorChannel.All)
+    edubitMotors.setServoPosition(ServoChannel.S1, 90)
+    basic.pause(200)
+    edubitMotors.disableServo(ServoChannel.All)
 })
 input.onButtonPressed(Button.B, function () {
     edubitMotors.runMotor(MotorChannel.M1, MotorDirection.Backward, edubitPotentioBit.readPotValue())
+    edubitMotors.setServoPosition(ServoChannel.S1, 180)
+    basic.pause(200)
+    edubitMotors.disableServo(ServoChannel.All)
 })
 basic.showIcon(IconNames.Yes)
+edubitMotors.disableServo(ServoChannel.All)
 basic.forever(function () {
     basic.showNumber(edubitPotentioBit.readPotValue())
 })
